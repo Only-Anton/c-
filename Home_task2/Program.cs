56872e216d3440126741012e6198ccd5da5e575e
Name <123@mail.ru>
2023-11-21 15:00:39 +0300

    Домашнее задание 2, все задачи

--- Home_task2/Program.cs
+++ Home_task2/Program.cs
@@ -25,5 +25,34 @@
 // if (x>0 && y<0) {Console.WriteLine("Ваши координаты ы четверти: IV");}
 
 
-Console.Write("Введите координату Х: ");
-int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите большое число: ");
+// int count = Convert.ToInt32(Console.ReadLine());
+// while (count>999)
+// {
+//     count/= 10;
+// }
+// if (count < 100) {Console.WriteLine("Введите число побольше");}
+// else 
+// {    
+//     count = count % 10;
+//     Console.WriteLine($"третье число сначала: {count}");
+// }
+
+Console.Write("Введите большое число: ");
+int count = Convert.ToInt32(Console.ReadLine());
+int n=0; // Счетчик для цикла
+int dooble = count; // Дублируем заданное число, чтобы не потерять
+while (dooble > 9) // 125, 12, 1
+{
+    dooble/=10;
+    n++;            // 1  2   
+}
+while (n>=0)
+{
+    int del = Convert.ToInt32(Math.Pow(10,n));// Добавили еще одну переменную, 
+                                              //так как степень требует другую систему чисел
+    int numb=count/del;     // 125/100 = 1  25/10 = 2   5
+    count = count % del;          // 125 100 = 25   5   0 
+    Console.Write($"{numb},");         
+    n=n-1;                           
+}
