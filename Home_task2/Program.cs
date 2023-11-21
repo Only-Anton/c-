cf5d037fb679ab198f0dc67f11c161644f278a73
Name <123@mail.ru>
2023-11-21 13:31:44 +0300

    ДЗ 2 задачи 1+2

---
+++ Home_task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Console.Write("Введите первое число: ");
+// int numb1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int numb2 = Convert.ToInt32(Console.ReadLine());
+// if (numb1==numb2*numb2)
+// {
+//     Console.WriteLine("Yes!!!");
+// }
+// else if (numb2==numb1*numb1)
+// {
+//     Console.WriteLine("Yes!!!");
+// }
+// else Console.WriteLine("Noooo");
+// Задача 2 определение четверти по заданным координатам
+
+// Console.Write("Введите координату Х: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату Y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// if (x==0) { Console.WriteLine("х не должен ровняться нулю");}
+// if (y==0) {Console.WriteLine("y не должен ровняться нулю");}
+// if (x>0 && y>0) {Console.WriteLine("Ваши координаты ы четверти: I");}
+// if (x<0 && y>0) {Console.WriteLine("Ваши координаты ы четверти: II");}
+// if (x<0 && y<0) {Console.WriteLine("Ваши координаты ы четверти: III");}
+// if (x>0 && y<0) {Console.WriteLine("Ваши координаты ы четверти: IV");}
+
+
+Console.Write("Введите координату Х: ");
+int x = Convert.ToInt32(Console.ReadLine());
